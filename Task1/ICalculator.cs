5c2917decb0d51159bba6297070f0cac628085ea
v-king777 <v.kolpashchikov@gmail.com>
2021-03-11 00:20:02 +0300

    new file:   Task1/ICalculator.cs

---
+++ Task1/ICalculator.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1
+{
+    interface ICalculator
+    {
+        double Sum(double a, double b); // '+'
+
+        double Subtraction(double a, double b); // '-'
+
+        double Multiplication(double a, double b); // '*'
+
+        double Division(double a, double b); // '/'
+
+        void DisplayResult(double result); // '='
+
+        double ReadNumber();
+    }
+}
