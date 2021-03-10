15d57792cd27b6d68ed67b40f96a397fbe48f54c
v-king777 <v.kolpashchikov@gmail.com>
2021-03-11 00:19:47 +0300

    new file:   Task1/Calculator.cs

---
+++ Task1/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task1
+{
+    class Calculator : ICalculator
+    {
+        double ICalculator.Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        double ICalculator.Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        double ICalculator.Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        double ICalculator.Division(double a, double b)
+        {
+            return a / b;
+        }
+
+        void ICalculator.DisplayResult(double result)
+        {
+            Console.WriteLine("Результат: " + result);
+        }
+
+        double ICalculator.ReadNumber()
+        {
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
