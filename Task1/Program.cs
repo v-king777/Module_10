ebe3955652c5f982341e473ddf50da11f15c6710
v-king777 <v.kolpashchikov@gmail.com>
2021-03-11 00:20:15 +0300

    modified:   Task1/Program.cs

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,7 +6,45 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ICalculator calculator = new Calculator();
+
+            try
+            {
+                double result = 0;
+                double a = calculator.ReadNumber();
+                double b = calculator.ReadNumber();
+                string operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "+":
+                        result = calculator.Sum(a, b);
+                        break;
+
+                    case "-":
+                        result = calculator.Subtraction(a, b);
+                        break;
+
+                    case "*":
+                        result = calculator.Multiplication(a, b);
+                        break;
+
+                    case "/":
+                        result = calculator.Division(a, b);
+                        break;
+
+                    default:
+                        Console.WriteLine("Неизвестная операция");
+                        break;
+                }
+
+                calculator.DisplayResult(result);
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректный ввод числа");
+            }
         }
     }
 }
