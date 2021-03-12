335cd801f4addf5299ee712a83469778b9bb3a09
v-king777 <77056113+v-king777@users.noreply.github.com>
2021-03-12 15:17:25 +0300

    Create Calculator.cs

---
+++ Task2/Calculator.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading;
+
+namespace Task2
+{
+    class Calculator : ICalculator
+    {
+        Logger Logger { get; }
+
+        public Calculator(Logger logger)
+        {
+            Logger = logger;
+        }
+       
+       double ICalculator.Sum(double a, double b)
+        {
+            Logger.Event("Выполняется операция сложения . . .");
+            Thread.Sleep(3000);
+            return a + b;
+        }
+
+        double ICalculator.Subtraction(double a, double b)
+        {
+            Logger.Event("Выполняется операция вычитания . . .");
+            Thread.Sleep(3000);
+            return a - b;
+        }
+
+        double ICalculator.Multiplication(double a, double b)
+        {
+            Logger.Event("Выполняется операция умножения . . .");
+            Thread.Sleep(3000);
+            return a * b;
+        }
+
+        double ICalculator.Division(double a, double b)
+        {
+            Logger.Event("Выполняется операция деления . . .");
+            Thread.Sleep(3000);
+            return a / b;
+        }
+
+        void ICalculator.DisplayResult(double result)
+        {
+            Console.WriteLine("Результат: " + result);
+        }
+
+        double ICalculator.ReadNumber()
+        {
+            string read = Console.ReadLine();
+            Logger.Event("Выполняется обрабатка данных . . .");
+            Thread.Sleep(3000);
+            return Convert.ToDouble(read);
+        }
+    }
+}
