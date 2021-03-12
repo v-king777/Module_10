e5040bf8afb51efaf59a9888fd2a8ba6ee1b2618
v-king777 <77056113+v-king777@users.noreply.github.com>
2021-03-12 15:19:46 +0300

    Create Logger.cs

---
+++ Task2/Logger.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Task2
+{
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
