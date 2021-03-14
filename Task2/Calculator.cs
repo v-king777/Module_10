44c10985c8014d685c21a58504b40939f819b37a
v-king777 <v.kolpashchikov@gmail.com>
2021-03-14 18:48:11 +0300

    modified:   Task2/Calculator.cs

--- Task2/Calculator.cs
+++ Task2/Calculator.cs
@@ -1,17 +1,37 @@
+// <copyright file="Calculator.cs" company="MyCompany.com">
+// Copyright (c) 2021. All rights reserved.
+// </copyright>
+// <author>v-king777</author>
 using System;
 using System.Threading;
 
 namespace Task2
 {
-    class Calculator : ICalculator
+    /// <summary>
+    /// Calculator class
+    /// </summary>
+    internal class Calculator : ICalculator
     {
-        Logger Logger { get; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Calculator"/> class
+        /// </summary>
+        /// <param name="logger">Variable of type Logger</param>
         public Calculator(Logger logger)
         {
             Logger = logger;
         }
 
+        /// <summary>
+        /// Gets a variable of type Logger
+        /// </summary>
+        private Logger Logger { get; }
+
+        /// <summary>
+        /// Performs an addition operation
+        /// </summary>
+        /// <param name="a">Variable one of type Double</param>
+        /// <param name="b">Variable two of type Double</param>
+        /// <returns>The result of adding numbers a and b</returns>
         double ICalculator.Sum(double a, double b)
         {
             Logger.Event("Выполняется операция сложения . . .");
@@ -19,6 +39,12 @@ namespace Task2
             return a + b;
         }
 
+        /// <summary>
+        /// Performs a subtraction operation
+        /// </summary>
+        /// <param name="a">Variable one of type Double</param>
+        /// <param name="b">Variable two of type Double</param>
+        /// <returns>The result of subtracting the numbers a and b</returns>
         double ICalculator.Subtraction(double a, double b)
         {
             Logger.Event("Выполняется операция вычитания . . .");
@@ -26,6 +52,12 @@ namespace Task2
             return a - b;
         }
 
+        /// <summary>
+        /// Performs a multiplication operation
+        /// </summary>
+        /// <param name="a">Variable one of type Double</param>
+        /// <param name="b">Variable two of type Double</param>
+        /// <returns>The result of the multiplication of numbers a and b</returns>
         double ICalculator.Multiplication(double a, double b)
         {
             Logger.Event("Выполняется операция умножения . . .");
@@ -33,6 +65,12 @@ namespace Task2
             return a * b;
         }
 
+        /// <summary>
+        /// Performs division operation
+        /// </summary>
+        /// <param name="a">Variable one of type Double</param>
+        /// <param name="b">Variable two of type Double</param>
+        /// <returns>The result of dividing the numbers a and b</returns>
         double ICalculator.Division(double a, double b)
         {
             Logger.Event("Выполняется операция деления . . .");
@@ -40,11 +78,19 @@ namespace Task2
             return a / b;
         }
 
+        /// <summary>
+        /// Displays the result of an arithmetic operation
+        /// </summary>
+        /// <param name="result">The result of an arithmetic operation</param>
         void ICalculator.DisplayResult(double result)
         {
             Console.WriteLine("Результат: " + result);
         }
 
+        /// <summary>
+        /// Reads user input
+        /// </summary>
+        /// <returns>Variable of type Double</returns>
         double ICalculator.ReadNumber()
         {
             string read = Console.ReadLine();
