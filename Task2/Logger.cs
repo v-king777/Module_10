6f31ee5aa96413bc75ab4e56c5e5e6b24e0d0635
v-king777 <v.kolpashchikov@gmail.com>
2021-03-14 18:48:56 +0300

    modified:   Task2/Logger.cs

--- Task2/Logger.cs
+++ Task2/Logger.cs
@@ -1,9 +1,20 @@
+// <copyright file="Logger.cs" company="MyCompany.com">
+// Copyright (c) 2021. All rights reserved.
+// </copyright>
+// <author>v-king777</author>
 using System;
 
 namespace Task2
 {
-    public class Logger : ILogger
+    /// <summary>
+    /// Logger class
+    /// </summary>
+    internal class Logger : ILogger
     {
+        /// <summary>
+        /// Creates an event message
+        /// </summary>
+        /// <param name="message">Event message</param>
         public void Error(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -11,6 +22,10 @@ namespace Task2
             Console.ForegroundColor = ConsoleColor.White;
         }
 
+        /// <summary>
+        /// Creates an error message
+        /// </summary>
+        /// <param name="message">Error message</param>
         public void Event(string message)
         {
             Console.ForegroundColor = ConsoleColor.Blue;
