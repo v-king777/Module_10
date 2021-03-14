6171331ab2a1aa12bee88d1544724a198a9cbd2d
v-king777 <v.kolpashchikov@gmail.com>
2021-03-14 18:49:08 +0300

    modified:   Task2/MyException.cs

--- Task2/MyException.cs
+++ Task2/MyException.cs
@@ -1,9 +1,22 @@
-﻿using System;
+﻿// <copyright file="MyException.cs" company="MyCompany.com">
+// Copyright (c) 2021. All rights reserved.
+// </copyright>
+// <author>v-king777</author>
+using System;
 
 namespace Task2
 {
-    class MyException : Exception
+    /// <summary>
+    /// MyException class
+    /// </summary>
+    internal class MyException : Exception
     {
-        public MyException(string message) : base(message) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyException"/> class
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public MyException(string message) : base(message)
+        {
+        }
     }
 }
