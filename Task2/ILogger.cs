db5695f8b18bbceecb3ff556149083928f3772fc
v-king777 <v.kolpashchikov@gmail.com>
2021-03-14 18:48:40 +0300

    modified:   Task2/ILogger.cs

--- Task2/ILogger.cs
+++ Task2/ILogger.cs
@@ -1,9 +1,24 @@
+// <copyright file="ILogger.cs" company="MyCompany.com">
+// Copyright (c) 2021. All rights reserved.
+// </copyright>
+// <author>v-king777</author>
 namespace Task2
 {
-    public interface ILogger
+    /// <summary>
+    /// Logger interface
+    /// </summary>
+    internal interface ILogger
     {
+        /// <summary>
+        /// Creates an event message
+        /// </summary>
+        /// <param name="message">Event message</param>
         void Event(string message);
 
+        /// <summary>
+        /// Creates an error message
+        /// </summary>
+        /// <param name="message">Error message</param>
         void Error(string message);
     }
 }
